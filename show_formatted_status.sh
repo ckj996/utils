829f6c9b9d5e3e24aebb3889f927e74fd57a9c0b
Kaijie Chen <ckj996@gmail.com>
2017-06-08 22:27:06 +0800

    use single sed

--- show_formatted_status.sh
+++ show_formatted_status.sh
@@ -4,8 +4,8 @@
 # 2017/06/08, ckj
 
 cat status.json | sed -e 's/^{/{\n\t/'              \
-                | sed -e 's/}}$/\n\t}\n}\n/'        \
-                | sed -e 's/: {/:\n\t{\n\t\t/g'     \
-                | sed -e 's/}, /\n\t},\n\t/g'       \
-                | sed -e 's/, /,\n\t\t/g'           \
-                | sed -e 's/\t/    /g'
+                      -e 's/}}$/\n\t}\n}\n/'        \
+                      -e 's/: {/:\n\t{\n\t\t/g'     \
+                      -e 's/}, /\n\t},\n\t/g'       \
+                      -e 's/, /,\n\t\t/g'           \
+                      -e 's/\t/    /g'
